ca4e119c74b709ea556ee67390c28ef02f70d017
Janno Põldma <janno.poldma@gmail.com>
2017-11-10 10:57:59 +0200

    Refactor

---
+++ src/Rik.StatusPage/Configuration/ConfigurationExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Text.RegularExpressions;
+
+namespace Rik.StatusPage.Configuration
+{
+    public static class ConfigurationExtensions
+    {
+        private static readonly Regex appSettingsReferencePattern = new Regex("\\${(.*?)}");
+
+        public static string GetAppSettingsOrValue(this string configurationValue)
+        {
+            var match = appSettingsReferencePattern.Match(configurationValue);
+
+            if (!match.Success)
+                return configurationValue;
+
+            var connectionString = match.Groups[1].Value;
+            return ConfigurationManager.AppSettings[connectionString];
+        }
+    }
+}
--- src/Rik.StatusPage/Configuration/StatusProviderConfigurationElement.cs
+++ src/Rik.StatusPage/Configuration/StatusProviderConfigurationElement.cs
@@ -1,5 +1,4 @@
 ﻿using System.Configuration;
-using System.Text.RegularExpressions;
 
 namespace Rik.StatusPage.Configuration
 {
@@ -8,8 +7,8 @@ namespace Rik.StatusPage.Configuration
         [ConfigurationProperty("name", IsRequired = true)]
         public string Name
         {
-            get { return (string) this["name"]; }
-            set { this["name"] = value; }
+            get => (string) this["name"];
+            set => this["name"] = value;
         }
 
         [ConfigurationProperty("type", IsRequired = true)]
@@ -18,158 +17,78 @@ namespace Rik.StatusPage.Configuration
         [ConfigurationProperty("connectionString", IsRequired = false)]
         public string ConnectionString
         {
-            get
-            {
-                var rawValue = (string) this["connectionString"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var connectionString = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[connectionString];
-            }
-            set { this["connectionString"] = value; }
+            get => ((string)this["connectionString"]).GetAppSettingsOrValue();
+            set => this["connectionString"] = value;
         }
 
         [ConfigurationProperty("storagePath", IsRequired = false)]
         public string StoragePath
         {
-            get
-            {
-                var rawValue = (string) this["storagePath"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var storagePath = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[storagePath];
-            }
-            set { this["storagePath"] = value; }
+            get => ((string)this["storagePath"]).GetAppSettingsOrValue();
+            set => this["storagePath"] = value;
         }
 
         [ConfigurationProperty("requireRead", IsRequired = false, DefaultValue = false)]
         public bool RequireRead
         {
-            get { return (bool) this["requireRead"]; }
-            set { this["requireRead"] = value; }
+            get => (bool)this["requireRead"];
+            set => this["requireRead"] = value;
         }
 
         [ConfigurationProperty("requireWrite", IsRequired = false, DefaultValue = false)]
         public bool RequireWrite
         {
-            get { return (bool) this["requireWrite"]; }
-            set { this["requireWrite"] = value; }
+            get => (bool)this["requireWrite"];
+            set => this["requireWrite"] = value;
         }
 
         [ConfigurationProperty("protocol", IsRequired = false)]
         public string Protocol
         {
-            get { return (string) this["protocol"]; }
-            set { this["protocol"] = value; }
+            get => (string)this["protocol"];
+            set => this["protocol"] = value;
         }
 
         [ConfigurationProperty("securityServer", IsRequired = false)]
         public string SecurityServer
         {
-            get
-            {
-                var rawValue = (string)this["securityServer"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var securityServer = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[securityServer];
-            }
-            set { this["securityServer"] = value; }
+            get => ((string)this["securityServer"]).GetAppSettingsOrValue();
+            set => this["securityServer"] = value;
         }
 
         [ConfigurationProperty("producerName", IsRequired = false)]
         public string ProducerName
         {
-            get
-            {
-                var rawValue = (string)this["producerName"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var producerName = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[producerName];
-            }
-            set { this["producerName"] = value; }
+            get => ((string)this["producerName"]).GetAppSettingsOrValue();
+            set => this["producerName"] = value;
         }
 
         [ConfigurationProperty("consumer", IsRequired = false)]
         public string Consumer
         {
-            get
-            {
-                var rawValue = (string)this["consumer"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var consumer = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[consumer];
-            }
-            set { this["consumer"] = value; }
+            get => ((string)this["consumer"]).GetAppSettingsOrValue();
+            set => this["consumer"] = value;
         }
 
         [ConfigurationProperty("userId", IsRequired = false)]
         public string UserId
         {
-            get
-            {
-                var rawValue = (string)this["userId"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var userId = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[userId];
-            }
-            set { this["userId"] = value; }
+            get => ((string)this["userId"]).GetAppSettingsOrValue();
+            set => this["userId"] = value;
         }
 
         [ConfigurationProperty("url", IsRequired = false)]
         public string Url
         {
-            get
-            {
-                var rawValue = (string)this["url"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var url = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[url];
-            }
-            set { this["url"] = value; }
+            get => ((string)this["url"]).GetAppSettingsOrValue();
+            set => this["url"] = value;
         }
 
         [ConfigurationProperty("class", IsRequired = false)]
         public string Class
         {
-            get
-            {
-                var rawValue = (string)this["class"];
-                var match = Regex.Match(rawValue, "\\${(.*?)}");
-
-                if (!match.Success)
-                    return rawValue;
-
-                var clazz = match.Groups[1].Value;
-                return ConfigurationManager.AppSettings[clazz];
-            }
-            set { this["class"] = value; }
+            get => ((string)this["class"]).GetAppSettingsOrValue();
+            set => this["class"] = value;
         }
     }
 }
